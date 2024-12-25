ae3368e61a6d384685a879e66a1afa8e8caeb63e
Abhishauk <ukabhisha@gmail.com>
2024-12-25 11:26:18 +0530

    updation

--- server/src/routes/Admin/services/consultation/dividendRoute.js
+++ server/src/routes/Admin/services/consultation/dividendRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createDividend, editDividend } from "../../../../controllers/Admin/Services/consultation/dividendController.js";
+import { createDividend, editDividend } from "../../../../controllers/Admin/Services/Consultation/dividendController.js";
 const router = express.Router();
 
 
