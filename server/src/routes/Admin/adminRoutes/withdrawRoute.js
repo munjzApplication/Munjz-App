2949dedc05d65cc5770992366a3ce97d50275daa
Abhishauk <ukabhisha@gmail.com>
2024-12-25 11:52:41 +0530

    updation

--- server/src/routes/Admin/adminRoutes/withdrawRoute.js
+++ server/src/routes/Admin/adminRoutes/withdrawRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { updateWithdrawalStatus } from "../../../controllers/Admin/Services/consultation/withdrawRequestController.js";
+import { updateWithdrawalStatus } from "../../../controllers/Admin/Services/Consultation/withdrawRequestController.js";
 
 const router = express.Router();
 
