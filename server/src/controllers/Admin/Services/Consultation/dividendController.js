73e319a1755a10ea032e2333eb8b7193b1289d40
Abhishauk <ukabhisha@gmail.com>
2025-01-30 15:21:47 +0530

    admin divident updates

--- server/src/controllers/Admin/Services/Consultation/dividendController.js
+++ server/src/controllers/Admin/Services/Consultation/dividendController.js
@@ -1,5 +1,33 @@
 import DividendModel from "../../../../models/Admin/adminModels/dividendModel.js";
 
+export const checkCountry = async (req, res, next) => {
+  try {
+    const { countryCode } = req.body;
+
+    // Validate input
+    if (!countryCode) {
+      return res.status(400).json({
+        message: "Country code is required."
+      });
+    }
+
+
+    const dividendData = await DividendModel.findOne({ countryCode });
+
+    if (!dividendData) {
+      return res.status(200).json({
+        message: "Pricing data for this country not found."
+      });
+    }
+
+    return res.status(200).json({
+      message: "Pricing data for this country already exists.",
+      data:dividendData
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 export const createDividend = async (req, res, next) => {
   try {
     const { countryCode, rates } = req.body;
--- server/src/routes/Admin/services/consultation/dividendRoute.js
+++ server/src/routes/Admin/services/consultation/dividendRoute.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { createDividend, editDividend } from "../../../../controllers/Admin/Services/Consultation/dividendController.js";
+import { createDividend, editDividend,checkCountry } from "../../../../controllers/Admin/Services/Consultation/dividendController.js";
 const router = express.Router();
 
-
+router.post("/check-country",checkCountry);
 router.post("/set-dividend", createDividend);
 router.put("/dividend/:countryCode", editDividend);
 
