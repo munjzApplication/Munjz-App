058f612c56a04869c127c21eb9a8a8964439a700
Abhishauk <ukabhisha@gmail.com>
2025-01-31 13:11:41 +0530

    wallet updation

--- server/src/helper/dateFormatter.js
+++ server/src/helper/dateFormatter.js
@@ -9,12 +9,13 @@ export const formatDate = (isoDate) => {
     return `${day}.${month}.${year} ${hours}:${minutes}`;
   };
   
-export const formatMinutesToHM = (minutes) => {
-  const totalMinutes = Number(minutes); // Ensure it's a number
-  if (isNaN(totalMinutes)) return "00.00"; // Handle invalid values
+  export const formatMinutesToHM = (minutes) => {
+    const totalMinutes = Number(minutes); // Ensure it's a number
+    if (isNaN(totalMinutes)) return "00:00"; // Handle invalid values
+    
+    const hours = Math.floor(totalMinutes / 60); // Get hours
+    const remainingMinutes = totalMinutes % 60; // Get remaining minutes
+    
+    return `${hours.toString().padStart(2, "0")}:${remainingMinutes.toString().padStart(2, "0")}`; // HH:MM format
+  };
   
-  const hours = Math.floor(totalMinutes / 60); // Get hours
-  const remainingMinutes = totalMinutes % 60; // Get remaining minutes
-  
-  return `${hours.toString().padStart(2, "0")}.${remainingMinutes.toString().padStart(2, "0")}`; // HH.MM format
-};
