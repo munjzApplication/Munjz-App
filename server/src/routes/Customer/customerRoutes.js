93b4e8c8c6919a2a02730401296e04097590ce38
Abhishauk <ukabhisha@gmail.com>
2025-02-26 13:38:24 +0530

    updated commit

--- server/src/routes/Customer/customerRoutes.js
+++ server/src/routes/Customer/customerRoutes.js
@@ -29,7 +29,7 @@ router.use("/forgot-password", customerForgotRoute);
 router.use("/activity",authenticateUser, callActivity);
 router.use('/transaction',authenticateUser,transactionRoute);
 router.use('/favorite',authenticateUser,favRoute);
-router.use('/court',authenticateUser,courtServiceRoute);
+router.use('/court-service',authenticateUser,courtServiceRoute);
 router.use('/adminCourt-req-submit',authenticateUser,courtServiceReqRoute);
 router.use('/notary-service',authenticateUser,notaryServiceRoute);
 router.use('/adminNotary-req-submit',authenticateUser,notaryServiceReqRoute);
--- server/src/routes/Customer/services/courtServices/courtServiceRoute.js
+++ server/src/routes/Customer/services/courtServices/courtServiceRoute.js
@@ -9,7 +9,7 @@ import upload from "../../../../middlewares/fileUpload.js";
 const router = express.Router();
 
 router.get("/get-service/:country", getServices);
-router.post("/submit-court-service",upload.array("documents"),saveCourtServiceDetails);
+router.post("/register",upload.array("documents"),saveCourtServiceDetails);
 router.get("/get-cases",getAllCourtCases)
 
 export default router;
