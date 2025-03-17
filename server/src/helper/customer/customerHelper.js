c4f64d149c91dcd5444e84df6beb7d3440473f9e
Abhishauk <ukabhisha@gmail.com>
2025-03-17 12:30:28 +0530

    updated commit

--- server/src/helper/customer/customerHelper.js
+++ server/src/helper/customer/customerHelper.js
@@ -1,21 +1,19 @@
 import CustomerProfile from '../../models/Customer/customerModels/customerModel.js';
 
 export const generateCustomerUniqueId = async () => {
-    const latestCustomer = await CustomerProfile.findOne().sort({ creationDate: -1 });
-    console.log("latestCustomer",latestCustomer);
-    
-    let newId = 1;
-    
-    if (latestCustomer && latestCustomer.customerUniqueId) {
+  const latestCustomer = await CustomerProfile.findOne().sort({ _id: -1 }); // Sort by _id to get the latest entry
+  console.log("latestCustomer", latestCustomer);
+  
+  let newId = 1;
+  
+  if (latestCustomer && latestCustomer.customerUniqueId) {
       const lastIdNumber = parseInt(latestCustomer.customerUniqueId.replace("MCust", ""), 10);
-      console.log("lastIdNumber",lastIdNumber);
+      console.log("lastIdNumber", lastIdNumber);
       
       newId = lastIdNumber + 1;
 
-      console.log("newId",newId);
-      
-    }
-    
-    return `MCust${String(newId).padStart(2, '0')}`;
-  };
+      console.log("newId", newId);
+  }
   
+  return `MCust${String(newId).padStart(2, '0')}`;
+};
