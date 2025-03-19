682a79752af114cd142c29dd9e6bb8dee5333030
Abhishauk <ukabhisha@gmail.com>
2025-03-19 12:14:40 +0530

    courtservice document updated commit

--- server/src/routes/Admin/services/courtServices/courtCaseRoutes.js
+++ server/src/routes/Admin/services/courtServices/courtCaseRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { editCourtCase } from "../../../../controllers/Admin/Services/CourtService/courtCaseController.js";
+import { adminSubmittedDoc } from "../../../../controllers/Admin/Services/CourtService/courtRequestController.js";
 const router = express.Router();
 
 router.put("/edit-case/:caseId",editCourtCase);
+router.post('/additional-docs/:caseId',upload.array('file',5),adminSubmittedDoc)
 
 
 export default router;
--- server/src/routes/Admin/services/courtServices/courtServiceRequestRoutes.js
+++ server/src/routes/Admin/services/courtServices/courtServiceRequestRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { requestDocument, reviewDocument, requestAdditionalPayment,adminSubmittedDoc } from '../../../../controllers/Admin/Services/CourtService/courtRequestController.js';
+import { requestDocument, reviewDocument, requestAdditionalPayment } from '../../../../controllers/Admin/Services/CourtService/courtRequestController.js';
 import upload from '../../../../middlewares/fileUpload.js';
 const router = express.Router();
 
 
 // Document Requests
-router.post('/admin-submittedDocument/:caseId',upload.array('file',5),adminSubmittedDoc)
+
 router.post('/documents/request/:caseId', requestDocument);
 router.patch('/documents/review/:documentId', reviewDocument);
 
