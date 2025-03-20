f2818892cb0ffee7fc44024a0083460e40745e25
Abhishauk <ukabhisha@gmail.com>
2025-03-20 14:23:56 +0530

    courtservice document updated commit

--- server/src/controllers/Customer/services/courtService/getCourtCaseController.js
+++ server/src/controllers/Customer/services/courtService/getCourtCaseController.js
@@ -43,7 +43,6 @@ export const getCaseDetails = async (req, res, next) => {
         }));
 
         res.status(200).json({
-            success: true,
             data: {
                 ...caseDetails,
                 documents,
