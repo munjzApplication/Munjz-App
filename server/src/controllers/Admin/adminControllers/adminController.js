88b6de9bc5fc40f0b24940c69504ce0372660663
Abhishauk <ukabhisha@gmail.com>
2025-04-29 10:16:44 +0530

    Updated

--- server/src/controllers/Admin/adminControllers/adminController.js
+++ server/src/controllers/Admin/adminControllers/adminController.js
@@ -5,12 +5,11 @@ export const Login = async (req, res, next) => {
   const { username, password } = req.body;
 
   try {
-    const admin = await Admin.findOne({ username });
+    const admin = await Admin.findOne({ user: username });
 
-console.log("Admin found:", admin); // Debugging line
+    console.log("Admin found:", admin);
 
     if (!admin) return res.status(404).json({ message: "Admin not found" });
-
 
     if (admin.password !== password) {
       return res.status(401).json({ message: "Invalid password" });
@@ -21,7 +20,6 @@ console.log("Admin found:", admin); // Debugging line
       process.env.JWT_SECRET,
       { expiresIn: process.env.JWT_EXPIRES_IN }
     );
-
 
     res.status(200).json({ message: "Login successful", token });
   } catch (error) {
