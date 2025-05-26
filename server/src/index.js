729e8fd1b2ffe877c588559851aefc2964c8f8ba
Abhishauk <ukabhisha@gmail.com>
2025-05-26 19:41:00 +0530

    debug test

--- server/src/index.js
+++ server/src/index.js
@@ -52,6 +52,5 @@ setupSocket(server);
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-  console.log("DEBUG - Mongo URI:", process.env.MONGO_URL);
   connectDB();
 });
